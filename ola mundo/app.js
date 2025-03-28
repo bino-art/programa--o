ac985684aacfc02eec9161670cb8b2a1ac0b7792
bino-art <brunolb2009@gmail.com>
2025-03-28 19:47:51 -0300

    initial commit

---
+++ ola mundo/app.js
@@ -0,0 +1,14 @@
+console.log("Olá, Mundo!");
+let message
+message= "hello"
+alert(message)
+let user = 'Bino'
+const mybirthday = '03.08.1996'
+let admin, name
+name = 'Bino'
+admin = name
+alert (admin)
+let OurPlanetName = 'earth'
+let CurrentUserName = 'Bino'
+const age = somecode(mybirthday)
+alert(age)
